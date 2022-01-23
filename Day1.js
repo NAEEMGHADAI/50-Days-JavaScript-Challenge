549ea57f8e6d8e7ffc6ab1439e7076cf5cfa15eb
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-01-23 21:52:46 +0530

    Questions in the comments

--- Day1.js
+++ Day1.js
@@ -1,3 +1,8 @@
+// Function which returns a random number in the given range
+// Create a function which returns a random number in the given range of values both inclusive
+// randomNumberGeneratorInRange(10, 50) should return a number between 10-50 (inclusive)
+// randomNumberGeneratorInRange(100, 200) should return a number between 100-200 (inclusive)
+
 function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
 	return Math.floor(Math.random() * (rangeEnd - rangeStart + 1) + rangeStart);
 }
