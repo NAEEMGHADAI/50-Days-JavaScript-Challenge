5c64533c944a532aafa793006ee2122540bb341b
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-01-25 23:00:44 +0530

    Day 5

--- Day5.js
+++ Day5.js
@@ -1,3 +1,14 @@
+/*
+Write a function which accepts a string argument and returns the count of characters between the first and last character 'X'
+indexOf and lastIndexOf are the methods on String which returns the position of the given string in the input string from start and end respectively
+If the match is not found, these methods return -1
+
+getTheGapX('XeroX') returns 4
+getTheGapX('Xamarin') returns 0
+getTheGapX('JavaScript') returns -1
+getTheGapX("F(X) !== G(X) !== F(X)") returns 18
+*/
+
 const str = "JavaScript";
 
 function getTheGapX(str) {
