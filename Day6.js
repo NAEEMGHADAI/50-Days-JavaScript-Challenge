63ab8dc3a154a10be3e39ff83b5dcf8f0f8b0f54
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-01-26 22:18:31 +0530

    Day 6

--- Day6.js
+++ Day6.js
@@ -1,3 +1,11 @@
+/*
+Write a function to truncate a string to a certain number of words
+Truncate a string to a certain number of words
+
+truncateWithWordLimit("JavaScript is simple", 3) returns "JavaScript is simple"
+truncateWithWordLimit("Codedamn is the best place to learn to code", 5) returns "Codedamn is the best place"
+*/
+
 const str = "JavaScript is simple but not easy to master";
 const wordLimit = 3;
 
