126ac25345624111a18e798e2e80b70ff73c6329
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-02-08 19:29:45 +0530

    Day 20

---
+++ Day20/Day20.js
@@ -0,0 +1,14 @@
+/*
+Add two numbers
+Instructions
+Add two numbers
+
+CHALLENGES (2/2 DONE)
+
+add Two Numbers
+JUST ADD TWO NUMBERS! YAAAY
+
+*/
+const addTwoNumbers = (a, b) => {
+	return a + b;
+};
