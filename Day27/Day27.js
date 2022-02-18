d9efa348d5669bb9ed0f28fd085de403fc0b1653
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-02-18 13:25:52 +0530

    Day 27

--- Day27/Day27.js
+++ Day27/Day27.js
@@ -1,3 +1,19 @@
+/*
+Vowel Count
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels
+
+The input can consit of Lower case and upper case letters so make sure to count both of them.
+
+CHALLENGES (3/3 DONE)
+
+getCount("abracadabra") should return 5
+getCount("THe Strings are SOO COOL") should return 8
+getCount("There exists only 5 vowels") should return 7
+
+*/
+
 function getCount(str) {
 	let vowelsCount = 0;
 	let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
