05cd015a7af11823f372a91332bb657913fe704a
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-03-02 19:10:39 +0530

    Day 34

--- Day34/Day34.js
+++ Day34/Day34.js
@@ -1,3 +1,15 @@
+/*
+Write a program to find the most frequent item of an array
+Input
+const arr = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+Output
+a 5
+CHALLENGES (0/2 DONE)
+
+mostFreq([1, 2, 2, 4, 5, 6, 6]) returns 2 2
+mostFreq([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]) returns a 5
+*/
+
 function mostFreq(arr) {
 	const counts = {};
 	arr.forEach(function (x) {
